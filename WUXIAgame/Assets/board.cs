3b53d6a8e5f5810ff804914345febcdca801d612
hl367 <73536843+hl367@users.noreply.github.com>
2021-10-08 00:18:31 +0800

    地點判定可以用了

--- WUXIAgame/Assets/board.cs
+++ WUXIAgame/Assets/board.cs
@@ -4,21 +4,29 @@ using UnityEngine;
 
 public class board : MonoBehaviour
 {
-    {
+    
 
-    // [SerializeField] int[] board = new int[5];
-    [SerializeField] List<int> theboard = new List<int>(1, 2, 3);
+    [SerializeField] int[] theboard;
+   
+    //  [SerializeField] List<int> theboard = new List<int>();
     void Start()
     {
         dicevalues = new int[1];
+        theboard = new int[]{ 1, 1, 1, 2, 3 , 1, 1};
+       /* 
+        theboard.Add(1);
+        theboard.Add(2);
+        theboard.Add(3);
+        */
     }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
             rolldice();
-            findlocation();
+            
         }
+        findlocation();
     }
 
     public int[] dicevalues;
@@ -32,25 +40,25 @@ public class board : MonoBehaviour
             dicetotal += dicevalues[i];
         }
 
-
     }
 
 
     public void findlocation()
     {
-        /* if (board[dicetotal] == 1)
+        
+        if (theboard[dicetotal] == 1)
          {
              Debug.Log("1");
          }
-         if (board[dicetotal] == 2)
+         if (theboard[dicetotal] == 2)
          {
              Debug.Log("2");
          }
-         if (board[dicetotal] == 3)
+         if (theboard[dicetotal] ==  3)
          {
              Debug.Log("3");
-         }*/
+         }
     }
 
-
+   
 }
